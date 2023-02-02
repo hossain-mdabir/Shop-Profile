cbfa6bbc366bc9b769583e68d56942b8127bc849
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-02-03 00:25:35 +0600

    Just created the project and added static shop name and address.

--- Shop Profile/ContentView.swift
+++ Shop Profile/ContentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HomeView()
         }
-        .padding()
+        .navigationTitle("Plain Notes")
+        .navigationBarTitleDisplayMode(.automatic)
+        .navigationViewStyle(.stack)
     }
 }
 
@@ -24,3 +23,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
