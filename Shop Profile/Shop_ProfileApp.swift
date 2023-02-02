323a853ddc06af277b310e1b66e80ce4959b4f0f
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-02-03 00:23:37 +0600

    Initial Commit

---
+++ Shop Profile/Shop_ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shop_ProfileApp.swift
+//  Shop Profile
+//
+//  Created by Md Abir Hossain on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Shop_ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shop ProfileUITests/Shop_ProfileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Shop_ProfileUITestsLaunchTests.swift
+//  Shop ProfileUITests
+//
+//  Created by Md Abir Hossain on 3/2/23.
+//
+
+import XCTest
+
+final class Shop_ProfileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
